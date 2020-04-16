c32f8a17b2dec4a229c37b7a9f91eeebcf3f73ae
leejiyoon7 <leejiyoon7@naver.com>
2020-04-16 16:53:27 +0900

    실습일지1 수정1

--- homework1.js
+++ homework1.js
@@ -4,22 +4,6 @@ const GREEN = 28; // 물리핀번호: 38
 const RED = 27; // 물리핀번호: 36
 var count = 0; // 순서를 카운팅하는 변수
 
-const AllOn = function ( ) {
-	gpio.digitalWrite(GREEN, 1);
-	gpio.digitalWrite(BLUE, 1);
-	gpio.digitalWrite(RED, 1);
-	console.log("Node: ALL on");
-	setTimeout(Alloff, 1000);
-}
-
-const AllOff = function ( ) {
-	gpio.digitalWrite(GREEN, 0);
-	gpio.digitalWrite(BLUE, 0);
-	gpio.digitalWrite(RED, 0);
-	console.log("Node: ALL off");
-}
-
-
 const TimeOutHandler = functon ( ) {
 switch (count % 4){
 case 0: gpio.digitalWrite (GREEN, 1); // 초록 on
@@ -62,6 +46,21 @@ default: break;
 }
 count++; // 순서증가
 setTimeout(TimeOutHandler, 1000); // 1초후 이벤트
+}
+
+const AllOn = function ( ) {
+	gpio.digitalWrite(GREEN, 1);
+	gpio.digitalWrite(BLUE, 1);
+	gpio.digitalWrite(RED, 1);
+	console.log("Node: ALL on");
+	setTimeout(Alloff, 500);
+}
+
+const AllOff = function ( ) {
+	gpio.digitalWrite(GREEN, 0);
+	gpio.digitalWrite(BLUE, 0);
+	gpio.digitalWrite(RED, 0);
+	console.log("Node: ALL off");
 }
 
 process.on('SIGINT', function() {
