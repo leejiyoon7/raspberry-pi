3dddcf7bedcb525f7f486e1ed1867ec1459dbac6
leejiyoon7 <leejiyoon7@naver.com>
2020-04-23 21:22:27 +0900

    실습일지2-3 마지막8

--- homework2-3.js
+++ homework2-3.js
@@ -14,25 +14,31 @@ const CheckTouch = function() {
 		if ((count++ % 2) == 1) {
 			gpio.digitalWrite(RED, 1); gpio.digitalWrite(BLUE, 0);
 			console.log("Touch! 조도센서 활성화");
-			do {
-				var LightData = gpio. digitalRead(LIGHT);
-				if(!LightData)
-					light1 = 1;
-				else
-					light2 = -1;
-				if((light1 - light2) == 2) {
-					console.log("조도센서 밝기변화 감지!");
-					BuzzerTurnOn();
-					light1,light2 = 0;
-					}
-				} while((count % 2) == 0);
-			}
+			CheakLight();
+		}
 		else {
 			gpio.digitalWrite(RED, 0); gpio.digitalWrite(BLUE, 1);
 			console.log("Touch! 조도센서 비활성화");
 		}
 	}
 	setTimeout(CheckTouch,500);
+}
+
+const CheakLight = function() {
+	var LightData = gpio. digitalRead(LIGHT);
+	if(!LightData)
+		light1 = 1;
+	else
+		light2 = -1;
+	if((light1 - light2) == 2) {
+		console.log("조도센서 밝기변화 감지!");
+		BuzzerTurnOn();
+		light1,light2 = 0;
+	}
+	if((count % 2) == 1)
+	setTimeout (CheakLight,500);
+	else
+		break;
 }
 
 const BuzzerTurnOn = function() {
