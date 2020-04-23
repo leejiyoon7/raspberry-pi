985b273b5296c4b003afa742f9bf3d0822ba171c
leejiyoon7 <leejiyoon7@naver.com>
2020-04-23 16:10:17 +0900

    touch

---
+++ touch.js
@@ -0,0 +1,18 @@
+const gpio = require('node-wiring-pi');
+const TOUCH = 23;
+
+const CheckTouch = function( ) {
+var data = gpio.digitalRead(TOUCH);
+if (data)
+console.log("Nodejs: Touched !");
+setTimeout(CheckTouch, 300);
+}
+
+process.on('SIGINT', function() {
+console.log("Program Exit...");
+process.exit();
+});
+
+gpio.setup('wpi');
+gpio.pinMode(TOUCH, gpio.INPUT);
+setTimeout(CheckTouch, 50);
