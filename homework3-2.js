fd18ce4390b5a4b7e127d775669803de4bab1159
leejiyoon7 <leejiyoon7@naver.com>
2020-05-08 23:45:03 +0900

    homework3-2 1

---
+++ homework3-2.js
@@ -0,0 +1,21 @@
+const gpio = require('node-wiring-pi');
+const BUTTON = 24;
+const LED = 2;
+
+const DetectButton = function() {
+gpio.digitalWrite(LED, 1);
+gpio.delay(50);
+gpio.digitalWrite(LED, 0);
+}
+
+process.on('SIGINT', function() {
+gpio.digitalWrite(LED, 0);
+console.log("Program Exit...");
+process.exit();
+});
+
+gpio.wiringPiSetup();
+gpio.pinMode(BUTTON, gpio.INPUT);
+gpio.pinMode(LED, gpio.OUTPUT);
+console.log("이벤트방식: 버튼을 누르면 LED가 켜집니다.....");
+gpio.wiringPiISR(SOUND, gpio.INT_EDGE_RISING, DetectButton);
