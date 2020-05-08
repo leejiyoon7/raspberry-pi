611d6636be2d41d658a56ffc11d758b774dd685e
leejiyoon7 <leejiyoon7@naver.com>
2020-05-08 22:16:38 +0900

    pwm_sonic 최종

--- pwm_sonic.js
+++ pwm_sonic.js
@@ -1,9 +1,10 @@
 const gpio = require('node-wiring-pi');
 const TRIG = 27;
 const ECHO = 28;
-const LED = 29;
+const LED = 2;
 var startTime; // 초음파 송출시간
 var travelTime; // 초음파수신까지 경과시간
+
 const Triggering = function() {
 gpio.digitalWrite (TRIG, gpio.LOW);
 gpio.delayMicroseconds(2)
@@ -22,6 +23,7 @@ effect_led(distance);
 }
 setTimeout(Triggering, 200);
 }
+
 const effect_led = (dis) => {
 if ( dis < 5) gpio.softPwmWrite(LED, 100);
 else if (dis >= 5 && dis < 10) gpio.softPwmWrite(LED, 60);
@@ -30,3 +32,13 @@ else if (dis >= 20 && dis < 50) gpio.softPwmWrite(LED, 15);
 else if (dis >= 50 && dis < 100) gpio.softPwmWrite(LED, 7);
 else gpio.softPwmWrite (LED, 0);
 }
+
+process.on('SIGINT', function() {
+gpio.softPwmWrite(LED, 0);
+process.exit();
+});
+gpio.wiringPiSetup();
+gpio.pinMode(TRIG, gpio.OUTPUT);
+gpio.pinMode(ECHO, gpio.INPUT);
+gpio.softPwmCreate(LED, 1, 100);
+setImmediate(Triggering);
