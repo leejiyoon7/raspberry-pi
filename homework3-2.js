efa6753b3aa004374737def581e0d2d7ce553f59
leejiyoon7 <leejiyoon7@naver.com>
2020-05-10 03:05:29 +0900

    homework3-2 4

--- homework3-2.js
+++ homework3-2.js
@@ -2,16 +2,38 @@ const gpio = require('node-wiring-pi');
 const BUTTON = 24;
 const LED = 2;
 
+var chatteringTimer = 100;
+var chatteringCount  = 0;
+var keyState = HIGH;
+
 const DetectButton = function() {
-gpio.digitalWrite(LED, 1);
-gpio.delay(50);
-gpio.digitalWrite(LED, 0);
+ let data = gpio.digitalRead(BUTTON);
+
+  if(data == LOW){
+    if(keyState == HIGH){
+      if(chatteringCount == 0){
+        chatteringCount  = chatteringTimer;
+        gpio.digitalWrite(LED, 1);
+        keyState = data;
+     	}
+  		else {
+  		chatteringCount--;
+  		}
+ 	}
 }
+    else {
+    	if(keyState == LOW) {
+    		gpio.digitalWrite(LED, 0);
+    		keyState = data;
+    	}
+    }
+
 process.on('SIGINT', function() {
 gpio.digitalWrite(LED, 0);
 console.log("Program Exit...");
 process.exit();
 });
+
 gpio.wiringPiSetup();
 gpio.pinMode(BUTTON, gpio.INPUT);
 gpio.pinMode(LED, gpio.OUTPUT);
