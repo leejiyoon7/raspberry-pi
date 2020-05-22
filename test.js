933ce334bfb2f9525eb1761bec8fb0bae1a7289b
leejiyoon7 <leejiyoon7@naver.com>
2020-05-22 17:28:58 +0900

    test

--- test.js
+++ test.js
@@ -2,19 +2,19 @@ const gpio = require('node-wiring-pi');
 const LED = 2;
 
 const ON = function () {
-    gpio.degitalWrite(LED,1);
+    gpio.digitalWrite(LED,1);
     console.log("LED ON");
     settimeout(OFF,1000);
 }
 
 const OFF = function () {
-    gpio.degitalWrite(LED,0);
+    gpio.digitalWrite(LED,0);
     console.log("LED OFF");
     settimeout(ON,1000);
 }
 
 process.on('SIGINT', function() {
-    gpio.degitalWrite(LED,0);
+    gpio.digitalWrite(LED,0);
     console.log("Process Exit");
     process.exit();
 });
