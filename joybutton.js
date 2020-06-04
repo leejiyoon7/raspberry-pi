3f28aa61d386ff9a370abe9991a4dd0bd1956bd0
leejiyoon7 <leejiyoon7@naver.com>
2020-06-04 16:59:40 +0900

    homework 5-1

---
+++ joybutton.js
@@ -0,0 +1,115 @@
+const fs = require('fs');
+const http = require('http');
+const gpio = require('node-wiring-pi');
+const socketio = require('socket.io');
+const mcpadc = require('mcp-spi-adc');
+const JOYBUTTON = 26;
+const RED = 27;
+const GREEN = 28;
+const BLUE = 29;
+const CS_MCP3208 = 10 // Chip Enable(CE0) is set
+const VRY = 1 // ADC 1번째 채널선택=아날로그센서
+const SPI_SPEED = 1000000 // Clock Speed = 1Mhz
+var timerid, timeout=800; // 타이머제어용
+var yvalue = -1; // JoyStick Y 측정데이터 저장용
+var count =0;
+
+const joyy = mcpadc.openMcp3208(VRY, // 채널1 지정 (Y좌표)
+{ speedHz: SPI_SPEED }, // Clock속도 지정
+(err) => { // Callback함수 등록
+console.log("SPI 채널1 초기화완료!");
+if (err) console.log('채널1 초기화실패!(HW점검!)');
+});
+
+const JoyStickCheckButton = ( ) => {
+	let checkButtonData = gpio.digitalRead(JOYBUTTON);
+	if (! checkButtonData) {
+		gpio.digitalWrite(BLUE, 1);
+		JoyStick();
+	}
+}
+
+const JoyStick = ( ) => {
+
+	joyy.read((error, reading)=> {
+		console.log(" ◀ ▶ (%d)", reading.rawValue);
+		yvalue = reading.rawValue;
+	});
+
+	if(yvalue<50 || yvalue >4800) {
+		if ((count % 3) == 1) {
+			gpio.digitalWrite(RED, 0);
+			gpio.digitalWrite(GREEN, 0);
+			gpio.digitalWrite(BLUE, 1); 
+			BuzzerTurnOn();
+			count++;
+		}
+		else if ((count % 3) == 2){
+			gpio.digitalWrite(RED, 1);
+			gpio.digitalWrite(GREEN, 0);
+			gpio.digitalWrite(BLUE, 0); 
+			BuzzerTurnOn();
+			count++;
+		}
+		else {
+			gpio.digitalWrite(RED, 0);
+			gpio.digitalWrite(GREEN, 1);
+			gpio.digitalWrite(BLUE, 0); 
+			BuzzerTurnOn();
+			count++;
+		}
+	}
+
+	if (yvalue != -1){ // y값 읽었다면
+		io.sockets.emit('watch', yvalue);
+		yvalue = -1;
+	}
+
+	timerid = setTimeout(JoyStick, timeout);
+}
+
+process.on('SIGINT', () => {
+	joyx.close(() => { // mcp3208 연결해제
+		joyy.close(() => {
+			console.log('MCP-ADC가 해제되어,웹서버를 종료합니다');
+			console.log('LED를 끄고 프로그램을 종료합니다.');
+			gpio.digitalWrite(RED, 0);
+			gpio.digitalWrite(GREEN, 0);
+			gpio.digitalWrite(BLUE, 0);
+			process.exit();
+		});
+	});
+});
+
+const serverbody = (request, response) => {
+	fs.readFile('views/plotly_joy.html', 'utf8', (err, data) => {
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.end(data);
+	});
+};
+
+const server = http.createServer(serverbody);
+const io = socketio.listen(server);
+
+io.sockets.on('connection', function (socket) {
+	socket.on('startmsg', function (data) {
+		console.log('가동메시지 수신(측정주기:%d)! ', data);
+		timeout = data;
+		JoyStickCheckButton(); // 타이머가동(조이스틱 기능 활성화)
+	});
+
+	socket.on('stopmsg', function (data) {
+		console.log('중지메시지 수신!');
+		clearTimeout(timerid); // 측정용 타이머를 해제 시킴
+	});
+});
+
+server.listen(60001, () => {
+	gpio.wiringPiSetup();
+	gpio.pinMode(CS_MCP3208, gpio.OUTPUT);
+	gpio.pullUpDnControl(JOYBUTTON,gpio.PUD_UP);
+	console.log('-----------------------------------------');
+	console.log('조이스틱 제어용 웹서버');
+	console.log("웹브라우져 접속주소 : http://IP주소:60001/");
+	console.log('-----------------------------------------');
+});
