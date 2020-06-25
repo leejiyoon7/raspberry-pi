65f10767e09eae69e2ff1ec7575630dca2bec533
leejiyoon7 <leejiyoon7@naver.com>
2020-06-25 16:25:42 +0900

    블루투스

--- peri-led-v2.0.js
+++ peri-led-v2.0.js
@@ -42,15 +42,15 @@ ApproachCharacteristic.prototype.onWriteRequest =
 	callback(this.RESULT_SUCCESS); // central 기기로 응답(성공)을 전송
 };
 ApproachCharacteristic.prototype.onSubscribe = (maxValueSize, updateValueCallback) => {
-	console.log("----------------------------------------------------------------------------------------------");
+	console.log("-------------------------------------------------------------------");
 	console.log("블루투스> 상대방이 센서데이터 연속서비스 가입(Subscribe)요청합니다");
-	console.log("----------------------------------------------------------------------------------------------");
+	console.log("-------------------------------------------------------------------");
 	this._updateValueCallback = updateValueCallback; // 연속적으로 송신하는 콜백함수를 활성화시킴
 };
 ApproachCharacteristic.prototype.onUnsubscribe = (maxValueSize, updateValueCallback) => {
-	console.log("--------------------------------------------------------------------------------------------------");
+	console.log("-------------------------------------------------------------------");
 	console.log("블루투스> 상대방이 센서데이터 연속서비스 탈퇴(Unsubscribe)요청합니다");
-	console.log("--------------------------------------------------------------------------------------------------");
+	console.log("-------------------------------------------------------------------");
 	this._updateValueCallback = null; // 연속적으로 송신하는 콜백함수를 비활성화시킴
 };
 bleno.on('stateChange', (state) => {
@@ -104,7 +104,7 @@ setInterval(() => {
 	analog_data++; // 가상의 아날로그 센서데이터를 측정한다고 가정함(즉, 1초마다 1씩증가시켜 업데이트함)
 	approachCharacteristic._value = analog_data;
 	if (approachCharacteristic._updateValueCallback) {
-		console.log('블루투스 > 연속데이터 송신 : ${ approachCharacteristic._value }');
+		console.log(`블루투스 > 연속데이터 송신: ${approachCharacteristic._value}`);
 		const notificationBytes = Buffer.from(String(approachCharacteristic._value));
 		approachCharacteristic._updateValueCallback(notificationBytes);
 	}
